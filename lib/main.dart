093e7dbb9f45c2dfc854dab1f43ba14505f995c0
TanishaOhri <ohritanisha@gmail.com>
2021-05-11 02:10:53 +0530

    Tic Tac Toe

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tictactoe/Screens/GameScreen.dart';
+import 'package:tictactoe/Screens/home.dart';
+import 'theme/theme.dart';
+import 'theme/theme2.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      // theme: Theme1.base,
+      // darkTheme: Theme2.base,
       debugShowCheckedModeBanner: false,
       home: GameScreen(),
     ),
